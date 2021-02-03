fea3257c9fd98a2fa6ecf6603f450c4488a255c8
ashrafulislampro <ashrafulislam1659@gmail.com>
2021-02-03 08:50:46 -0800

    finish ultra-nation

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Country = (props) => {
     
-    const {name, region, flag, population} = props.country;
+    const {name, region, flag,capital, population} = props.country;
     const style={
         border: '1px solid crimson',
         margin: '10px',
         padding: '10px',
         borderRadius: '5px',
         width: '700px',
-        height: '380px'
+        height: '450px'
 
     }
     const flagStyle={
@@ -20,6 +20,7 @@ const Country = (props) => {
         <div style={style}>
             <h3>This is Country : {name}</h3>
             <img style={flagStyle} src={flag} alt=""/>
+            <p>Capital : {capital}</p>
             <p><small>Region : {region}</small></p>
             <p><small>Population : {population}</small></p>
             <button onClick={ () => handleAddCountry(props.country)}>Add Country</button>
